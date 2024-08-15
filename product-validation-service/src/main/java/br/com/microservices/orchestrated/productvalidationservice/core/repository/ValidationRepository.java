a6141b4f28143dcd13f63bc8314e7188c073d3e2
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-08-15 19:35:51 -0300

    feat: implementando método para existencia e busca dos campos orderId e transactionId

--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/repository/ValidationRepository.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/repository/ValidationRepository.java
@@ -4,6 +4,12 @@ import br.com.microservices.orchestrated.productvalidationservice.core.model.Val
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ValidationRepository extends JpaRepository<Validation, Integer> {
+
+    Boolean existsByOrderIdAndTransactionId(String orderId, String transactionId);
+
+    Optional<Validation> findByOrderIdAndTransactionId(String orderId, String transactionId);
 }
