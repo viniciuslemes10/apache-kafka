4f294199d67c5cebe0af98c197eb8203102f3156
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-08-27 17:13:22 -0300

    feat: criando lógica para atualizar o estoque

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/model/OrderInventory.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/model/OrderInventory.java
@@ -2,6 +2,7 @@ package br.com.microservices.orchestrated.inventoryservice.core.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "order_inventory")
