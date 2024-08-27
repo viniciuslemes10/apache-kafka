a962cdc54c0ab0329fb7ed2f6f2f65cac7957e53
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-08-27 14:12:05 -0300

    feat: criação da lógica para realizar pagamentos pendentes

---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
@@ -0,0 +1,81 @@
+package br.com.microservices.orchestrated.paymentservice.core.service;
+
+import br.com.microservices.orchestrated.paymentservice.core.dto.Event;
+import br.com.microservices.orchestrated.paymentservice.core.dto.OrderProducts;
+import br.com.microservices.orchestrated.paymentservice.core.model.Payment;
+import br.com.microservices.orchestrated.paymentservice.core.producer.KafkaProducer;
+import br.com.microservices.orchestrated.paymentservice.core.repository.PaymentRepository;
+import br.com.microservices.orchestrated.paymentservice.core.utils.JsonUtil;
+import br.com.microservices.orchestrated.productvalidationservice.config.exception.ValidationException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class PaymentService {
+    private static final String CURRENT_SOURCE = "PAYMENT_SERVICE";
+    private static final Double REDUCE_SUM_VALUE = 0.0;
+
+    private final JsonUtil jsonUtil;
+    private final KafkaProducer producer;
+    private final PaymentRepository repository;
+
+    public void realizePayment(Event event) {
+        try {
+            checkCurrentValidation(event);
+            createPendingPayment(event);
+        } catch(Exception ex) {
+            log.error("Error trying to make payment: " + ex);
+        }
+        producer.sendEvent(jsonUtil.toJson(event));
+    }
+
+    private void createPendingPayment(Event event) {
+        var totalAmount = calculateAmount(event);
+        var totalItems = calculateTotalItems(event);
+        var payment = Payment
+                .builder()
+                .orderId(event.getPayload().getId())
+                .transactionId(event.getTransactionId())
+                .totalAmount(totalAmount)
+                .totalItems(totalItems)
+                .build();
+        save(payment);
+        setEventAmountItems(event, payment);
+    }
+
+    private double calculateAmount(Event event) {
+        return event
+                .getPayload()
+                .getProducts()
+                .stream()
+                .map(product -> product.getQuantity() * product.getProduct().getUnitValue())
+                .reduce(REDUCE_SUM_VALUE, Double::sum);
+    }
+
+    private int calculateTotalItems(Event event) {
+        return event
+                .getPayload()
+                .getProducts()
+                .stream()
+                .map(OrderProducts::getQuantity)
+                .reduce(REDUCE_SUM_VALUE.intValue(), Integer::sum);
+    }
+
+    private void setEventAmountItems(Event event, Payment payment) {
+        event.getPayload().setTotalItems(payment.getTotalItems());
+        event.getPayload().setTotalAmount(payment.getTotalAmount());
+    }
+
+    private void save(Payment payment) {
+        repository.save(payment);
+    }
+
+    private void checkCurrentValidation(Event event) {
+        if(repository.existsByOrderIdAndTransactionId(event.getPayload().getId(), event.getTransactionId())) {
+            throw new ValidationException("There's another transactionId for this validation.");
+        }
+    }
+}
