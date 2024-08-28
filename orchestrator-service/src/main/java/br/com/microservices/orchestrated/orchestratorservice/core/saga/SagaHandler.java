b92b0ca111908b0221be59552c951e2289915bd6
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-08-28 16:18:22 -0300

    feat: finalizando orchestrador

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/saga/SagaHandler.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/saga/SagaHandler.java
@@ -1,13 +1,12 @@
 package br.com.microservices.orchestrated.orchestratorservice.core.saga;
 
 import static br.com.microservices.orchestrated.orchestratorservice.core.enuns.EEventSource.*;
-import static br.com.microservices.orchestrated.orchestratorservice.core.enuns.ESagaStatus.FAIL;
-import static br.com.microservices.orchestrated.orchestratorservice.core.enuns.ESagaStatus.SUCCESS;
+import static br.com.microservices.orchestrated.orchestratorservice.core.enuns.ESagaStatus.*;
 import static br.com.microservices.orchestrated.orchestratorservice.core.enuns.ETopics.*;
 
 public final class SagaHandler {
 
-    public SagaHandler() {
+    private SagaHandler() {
 
     }
 
@@ -15,15 +14,15 @@ public final class SagaHandler {
             { ORCHESTRATOR, SUCCESS, PRODUCT_VALIDATION_SUCCESS },
             { ORCHESTRATOR, FAIL, FINISH_FAIL },
 
-            { PRODUCT_VALIDATION_SERVICE, FAIL, PRODUCT_VALIDATION_FAIL },
+            { PRODUCT_VALIDATION_SERVICE, ROLLBACK_PENDING, PRODUCT_VALIDATION_FAIL },
             { PRODUCT_VALIDATION_SERVICE, FAIL, FINISH_FAIL },
             { PRODUCT_VALIDATION_SERVICE, SUCCESS, PAYMENT_SUCCESS },
 
-            { PAYMENT_SERVICE, FAIL, PAYMENT_FAIL },
+            { PAYMENT_SERVICE, ROLLBACK_PENDING, PAYMENT_FAIL },
             { PAYMENT_SERVICE, FAIL, PRODUCT_VALIDATION_FAIL },
             { PAYMENT_SERVICE, SUCCESS, INVENTORY_SUCCESS },
 
-            { INVENTORY_SERVICE, FAIL, INVENTORY_FAIL },
+            { INVENTORY_SERVICE, ROLLBACK_PENDING, INVENTORY_FAIL },
             { INVENTORY_SERVICE, FAIL, PAYMENT_FAIL },
             { INVENTORY_SERVICE, SUCCESS, FINISH_SUCCESS }
     };
